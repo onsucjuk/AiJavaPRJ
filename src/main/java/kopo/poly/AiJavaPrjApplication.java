30094303ab11816acf006ffd3dca4d8a809b1c90
JeongukChoi <onsucjuk@gmail.com>
2023-06-07 10:26:32 +0900

    단어 횟수 출력

--- src/main/java/kopo/poly/AiJavaPrjApplication.java
+++ src/main/java/kopo/poly/AiJavaPrjApplication.java
@@ -1,12 +1,16 @@
 package kopo.poly;
 
+import kopo.poly.dto.NlpDTO;
 import kopo.poly.dto.OcrDTO;
+import kopo.poly.service.INlpService;
 import kopo.poly.service.IOcrService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,6 +25,9 @@ public class AiJavaPrjApplication implements CommandLineRunner {
     // 메모리에 올라간 OcrService 객체를 ocrService 변수에 객체를 넣어주기
     // 즉 메모리에 올라간 데이터를 똑같은 타입의 객체(변수)로 받는 것
 
+    private final INlpService nlpService;
+    //자연어 처리
+
     public static void main(String[] args) {
 
         SpringApplication.run(AiJavaPrjApplication.class, args);
@@ -33,7 +40,7 @@ public class AiJavaPrjApplication implements CommandLineRunner {
         log.info("자바 프로그래밍 시작!!");
 
         String filePath = "image"; // 문자열을 인식할 이미지 파일 경로
-        String fileName = "sample02.jpg"; // 문자열을 인식할 이미지 파일 이름
+        String fileName = "sample01.jpg"; // 문자열을 인식할 이미지 파일 이름
         // 읽을 이미지 파일
 
         OcrDTO pDTO = new OcrDTO();
@@ -55,5 +62,41 @@ public class AiJavaPrjApplication implements CommandLineRunner {
 
         log.info("자바 프로그래밍 종료!!");
 
+        log.info("-------------------------------------------------------");
+        NlpDTO nlpDTO = nlpService.getPlainText(result);
+        log.info("형태소별 품사 분석 결과 : " + nlpDTO.getResult());
+
+
+        nlpDTO = nlpService.getNouns(result);
+        //명사 추출 결과
+
+        List<String> nouns = nlpDTO.getNouns();
+        // 명사 추출결과를 nouns 변수에 저장하기
+
+        Set<String> distinct = new HashSet<>(nouns);
+
+        log.info("중복 제거 수행 전 단어 수 : " + nouns.size());
+        log.info("중복 제거 수행 후 단어 수 : " + distinct.size());
+
+        Map<String, Integer> rMap = new HashMap<>();
+        // 단어, 빈도수 Map 구조로 저장을 위해 생성 <중복 제거 HashMap>
+
+        for(String s : distinct) {
+            int count = Collections.frequency(nouns,s);
+            // 단어 빈도수 [distinct<중복제거>에 속한 단어를 전체 단어에서 각 단어마다 count]
+            rMap.put(s, count);
+            // 단어, 빈도수 Map 구조로 저장
+
+            log.info(s + " : " + count); // 저장 결과 출력
+        }
+
+        //빈도수 결과 정렬
+        List<Map.Entry<String, Integer>> sortResult = new LinkedList<>(rMap.entrySet());
+        // 정렬을 위해 맵에 저장된 레코드 1개(키, 값)을 리스트 구조로 변경하기
+
+        Collections.sort(sortResult, (o1,o2) -> o2.getValue().compareTo(o1.getValue()));
+
+        log.info("가장 많이 사용된 단어는? : " + sortResult);
+        log.info("자바 프로그래밍 종료!!");
     }
 }
